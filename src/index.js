23ad0ff814aacbda05da5b26efe8c7a355fc48c2
Victor Rodas <victormatiasrodas100@gmail.com>
2023-04-02 16:00:26 -0400

    add autoplay mode to avoid downloads in testing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { clearConsole } from './utils/cli.tools.js'
+import { clearConsole } from './cli/utils/cli.general.tools.js'
 import {
   setupMainOption,
   handleSearchByName,
@@ -6,11 +6,24 @@ import {
 } from './cli/interactions.js'
 import { developMode } from './utils/tools.js'
 
+import { spinner } from '@clack/prompts'
+import { playAudio } from './core/audio.js'
+import { PATHS } from './config.js'
+
 const showError = console.error
 
 async function main () {
-  developMode(true)
+  developMode(process.argv[2] === '--dev')
   clearConsole()
+
+  if (process.argv[3] === '--autoplay') {
+    const loader = spinner()
+
+    loader.start('Playing in dev mode...')
+    playAudio({ path: PATHS.absolutes.audio('sample.mp3') })
+
+    return loader.stop()
+  }
 
   const mainOption = await setupMainOption()
 
