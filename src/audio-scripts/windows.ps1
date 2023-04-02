67991c4a6d52d386b3765802777de705daffcaa2
Victor Rodas <victormatiasrodas100@gmail.com>
2023-04-02 16:02:37 -0400

    restructure project

--- src/audio-scripts/windows.ps1
+++
@@ -1,5 +0,0 @@
-$MediaPlayer = [Windows.Media.Playback.MediaPlayer, Windows.Media, ContentType = WindowsRuntime]::New();
-$MediaPlayer.Source = [Windows.Media.Core.MediaSource]::CreateFromUri($args[0]);
-$MediaPlayer.Play();
-
-echo "Playing $args[0]";
