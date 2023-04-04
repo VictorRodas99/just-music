f71acf8ca2ff4b7734206de9caac02eee79457ad
Victor Rodas <victormatiasrodas100@gmail.com>
2023-04-04 12:09:15 -0400

    add timer to control songs + add event handlers to control media player

--- src/core/audio.js
+++ src/core/audio.js
@@ -1,17 +1,37 @@
 import { spawn } from 'node:child_process'
-import { otherSystemMessage } from '../utils/tools.js'
+import { otherSystemMessage, songDurationToMiliseconds, parseOutput } from '../utils/tools.js'
 import { cliErrorMessage } from '../cli/utils/cli.general.tools.js'
 import { PATHS } from '../config.js'
 import { getMediaPlayerAction } from '../cli/interactions.js'
 
-const playAudioForWindows = async (path) => {
+import { EventEmitter } from 'node:events'
+import Timer from '../utils/timer.js'
+import { restartPlayer, nextSongHandler } from '../utils/player.tools.js'
+
+export const mediaPlayerEventHandler = new EventEmitter()
+
+const playAudioForWindows = async (song) => {
   const scriptPath = PATHS.scripts('music-player.ps1')
-  const args = ['-ExecutionPolicy', 'Bypass', '-File', scriptPath, path]
+  const args = ['-ExecutionPolicy', 'Bypass', '-File', scriptPath, song.path]
 
   const mediaPlayerProcess = spawn('Powershell.exe', args)
 
+  mediaPlayerEventHandler.on('restart', () => restartPlayer(mediaPlayerProcess))
+
   mediaPlayerProcess.stdout.on('data', (data) => {
-    console.log(data.toString())
+    const mode = parseOutput(data.toString())
+    const isInPlaylistMode = global.sessionMode === 'playlist'
+
+    if (isInPlaylistMode) {
+      if (mode === 'pause') {
+        global.timer.pause()
+      } else if (mode === 'resume') {
+        global.timer.resume()
+      } else if (mode === 'nextsong') {
+        restartPlayer(mediaPlayerProcess)
+        nextSongHandler()
+      }
+    }
   })
 
   mediaPlayerProcess.stderr.on('data', (chunk) => {
@@ -28,16 +48,25 @@ const playAudioForWindows = async (path) => {
   process.exit(0)
 }
 
-const playAudioForLinux = (path) => {
+const playAudioForLinux = (song) => {
   cliErrorMessage('Linux not supported yet')
 }
 
-const playAudioForMacOs = (path) => {
+const playAudioForMacOs = (song) => {
   cliErrorMessage('Mac Os not supported yet!')
 }
 
 export function playAudio ({ path, song }) {
-  // const songDuration = songDurationToMiliseconds(song.duration)
+  const songDuration = songDurationToMiliseconds(song.duration)
+
+  if (global.sessionMode === 'playlist') {
+    // TODO: restart the timer if next event is triggered
+
+    // Call 'next' event when the song ends
+    global.timer = new Timer(() => {
+      mediaPlayerEventHandler.emit('next')
+    }, songDuration)
+  }
 
   const actionsForSystem = {
     win32: playAudioForWindows,
@@ -46,11 +75,5 @@ export function playAudio ({ path, song }) {
   }
 
   const play = actionsForSystem[process.platform] ?? otherSystemMessage
-  play(path)
-  /*
-  const player = actionsForSytem... rest of code
-  player().on('play', play)
-  player().on('close', resetApp)
-  player().on('next', someFunctionThatPlaysNextMusicIfExists)
-  */
+  play({ path, duration: songDuration })
 }
