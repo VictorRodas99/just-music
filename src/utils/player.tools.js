94ce9ec8f5ca11bd831dbbdbe77efe49070fa905
Victor Rodas <victormatiasrodas100@gmail.com>
2023-04-05 20:46:28 -0400

    refactor code

--- src/utils/player.tools.js
+++ src/utils/player.tools.js
@@ -1,14 +1,6 @@
-import { playRandomSongFrom } from '../cli/utils/playlist.js'
-
 export const restartTimer = (newDuration) => {
   if (global.timer) {
     global.timer.restart(newDuration)
-  }
-}
-
-export const nextSongHandler = () => {
-  if (global.playlistPlayOption === 'random') {
-    playRandomSongFrom(global.playlist)
   }
 }
 
