fc04fcc967d57982207ba13909f6e5e4be3842b3
Victor Rodas <victormatiasrodas100@gmail.com>
2023-04-11 15:51:42 -0400

    add function to format milliseconds in 'mm:ss'

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -78,6 +78,17 @@ export const parseOutput = (data) => {
   return data.replace(/[^a-zA-Z]/g, '')
 }
 
+export const formatTime = (milliseconds) => {
+  const seconds = Math.floor(milliseconds / 1000)
+  const minutes = Math.floor(seconds / 60)
+  const remainingSeconds = seconds % 60
+
+  const formattedMinutes = String(minutes).padStart(2, '0')
+  const formattedSeconds = String(remainingSeconds).padStart(2, '0')
+
+  return `${formattedMinutes}:${formattedSeconds}`
+}
+
 /**
  * The initial duration format must be in ```mm:ss```
  * @param {string} duration
